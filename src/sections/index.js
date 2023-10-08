2c0a1d69c1a4f9d8639ab5d8df82505134d1dba1
tchimze <programbetter@gmail.com>
2023-10-08 06:27:44 +0000

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import PopularProducts from  './PopularProducts';
+import Hero from "./Hero";
+import SpecialQuality from "./SpecialQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOfffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    PopularProducts,
+    Hero,
+    SpecialQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
